9ca5e217ca56ecdb9726ceaf975a6a0d588bff63
RakanEngineer <rakan.engineer@gmail.com>
2025-11-10 10:51:06 +0100

    Add main menu

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---HUVUDMENY!---");
+            Console.WriteLine("0. Avsluta programmet");
+            Console.WriteLine("1. Ungdom eller pensionär");
+            Console.WriteLine("2. Sällskapspris");
+            Console.WriteLine("----------------------");
+            Console.Write("Välj ett alternativ: ");
+            string input = Console.ReadLine();
+
         }
     }
 }

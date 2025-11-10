722147c836cbbc2293da51fc47270b2436e5edca
RakanEngineer <rakan.engineer@gmail.com>
2025-11-10 11:01:32 +0100

    Add while loop and switch statement

--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
-﻿namespace BioMenyApp
+﻿
+namespace BioMenyApp
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            do
+            {
+                ShowMainMenu();
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine("Avslutar programmet...");
+                        return;
+                    case "1":
+                        Console.WriteLine("Du valde Ungdom eller pensionär.");
+                        break;
+                    case "2":
+                        Console.WriteLine("Du valde Sällskapspris.");
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val, försök igen.");
+                        break;
+                }
+            }
+            while (true);
+        }
+
+        private static void ShowMainMenu()
         {
             Console.WriteLine("---HUVUDMENY!---");
             Console.WriteLine("0. Avsluta programmet");
@@ -10,8 +37,6 @@
             Console.WriteLine("2. Sällskapspris");
             Console.WriteLine("----------------------");
             Console.Write("Välj ett alternativ: ");
-            string input = Console.ReadLine();
-
         }
     }
 }

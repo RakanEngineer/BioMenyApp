fd1371f09e02e035971f604331f7569bfdd5c835
RakanEngineer <rakan.engineer@gmail.com>
2025-11-10 17:35:07 +0100

    add a group size and calculate price

--- Program.cs
+++ Program.cs
@@ -40,7 +40,42 @@ namespace BioMenyApp
                         }
                         break;
                     case "2":
-                        Console.WriteLine("Du valde Sällskapspris.");
+                        Console.WriteLine("Hur många vi är som ska gå på bio:");
+                        string groupSizeInput = Console.ReadLine();
+                        if (int.TryParse(groupSizeInput, out int groupSize))
+                        {
+                           int totalPrice = 0;
+                            for (int i = 0; i < groupSize; i++)
+                            {
+                                Console.WriteLine($"Ange ålder för person {i + 1}: ");
+                                string personAgeInput = Console.ReadLine();
+                                if (int.TryParse(personAgeInput, out int personAge))
+                                {
+                                    if (personAge < 20)
+                                    {
+                                        totalPrice += 80;
+                                    }
+                                    else if (personAge > 64)
+                                    {
+                                        totalPrice += 90;
+                                    }
+                                    else
+                                    {
+                                        totalPrice += 120;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ogiltig ålder, försök igen.");
+                                }
+                            }
+                            Console.WriteLine($"\nAntal personer: {groupSize}");
+                            Console.WriteLine($"Totalkostnad: {totalPrice}kr\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ogiltigt antal, försök igen.");
+                        }
                         break;
                     default:
                         Console.WriteLine("Ogiltigt val, försök igen.");
